5fe681d7de32f43b3a54312918b1fbf043b2d19b
Michael <enjoyweaver@gmail.com>
2024-03-23 13:10:55 -0500

    tailwind

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -37,8 +37,8 @@ export default async function IndexPage() {
 
   return (
     <>
-      <section className="quando-regular space-y-2 pb-8 pt-2 md:pb-12 md:pt-10 lg:py-15">
-        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+      <section className="quando-regular lg:py-15 space-y-2 pb-8 pt-2 md:pb-12 md:pt-10">
+        <div className="container flex max-w-5xl flex-col items-center gap-4 text-center">
           <Link
             href={siteConfig.links.twitter}
             className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
@@ -52,7 +52,7 @@ export default async function IndexPage() {
               Your browser does not support the video tag.
             </video>
           </div>
-          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+          <p className="max-w-2xl leading-normal text-muted-foreground sm:text-xl sm:leading-8">
             Creating a safe meme economy
           </p>
         </div>
