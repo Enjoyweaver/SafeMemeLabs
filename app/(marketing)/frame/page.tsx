5cc2aaa03e1589fecb6f4c20b3cabdd80e41a7c5
Michael <enjoyweaver@gmail.com>
2024-06-24 17:01:00 -0500

    frames

--- app/(marketing)/frame/page.tsx
+++ app/(marketing)/frame/page.tsx
@@ -1,87 +1,58 @@
-"use client"
-
-import { useEffect, useState } from "react"
+import type { Metadata } from "next"
 import Image from "next/image"
 import { getFrameMetadata } from "@coinbase/onchainkit"
 
 import styles from "./page.module.css"
 
-// Function to fetch the list of images
-const fetchImages = async () => {
-  const response = await fetch("/api/images")
-  const data = await response.json()
-  return data
-}
+// Frame metadata
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: "Mint Selected NFT",
+    },
+  ],
+  image: {
+    src: `${process.env.NEXT_PUBLIC_SITE_URL}/images/luckyducks/Adam_G.jpg`,
+  },
+  input: {
+    text: "Your Wallet Address",
+  },
+  postUrl: `${process.env.NEXT_PUBLIC_SITE_URL}/api/mint`,
+})
 
-// Function to fetch the minted count
-const fetchMintedCount = async () => {
-  const response = await fetch("/api/mint/count")
-  const data = await response.json()
-  return data.count
-}
-
+// Frame component
 const Frame = () => {
-  const [images, setImages] = useState<string[]>([])
-  const [currentIndex, setCurrentIndex] = useState(0)
-  const [mintedCount, setMintedCount] = useState(0)
-
-  useEffect(() => {
-    const initializeImages = async () => {
-      const fetchedImages = [
-        "Abigail.jpg",
-        "Adele.jpg",
-        "Clark Fable.jpg",
-        "Cliff.jpg",
-        "Destiny.jpg",
-        "Dole.jpg",
-        "Duck Warhol.jpg",
-        "John.jpg",
-        "Josephine.jpg",
-        "Miss Grace.jpg",
-        "Miss Tonya.jpg",
-        "Weatherby.jpg",
-        "Virgil.jpg",
-        "Swirly.jpg",
-        "Straw.jpg",
-      ]
-      setImages(fetchedImages)
-    }
-
-    const initializeMintedCount = async () => {
-      const count = await fetchMintedCount()
-      setMintedCount(count)
-    }
-
-    initializeImages()
-    initializeMintedCount()
-  }, [])
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
-  }
-
-  const handlePrev = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + images.length) % images.length
-    )
-  }
+  const images = [
+    "Abigail.jpg",
+    "Adele.jpg",
+    "Clark Fable.jpg",
+    "Cliff.jpg",
+    "Destiny.jpg",
+    "Dole.jpg",
+    "Duck Warhol.jpg",
+    "John.jpg",
+    "Josephine.jpg",
+    "Miss Grace.jpg",
+    "Miss Tonya.jpg",
+    "Weatherby.jpg",
+    "Virgil.jpg",
+    "Swirly.jpg",
+    "Straw.jpg",
+  ]
 
   return (
     <div className={styles.frameContainer}>
       <h1 className={styles.title}>Mint Your NFT with our Safe Frames</h1>
-      <p className={styles.counter}>NFTs Left to Mint: {777 - mintedCount}</p>
       <div className={styles.navButtons}>
         <button onClick={handlePrev} className={styles.navButton}>
           Previous
         </button>
-        {images.length > 0 && (
-          <Image
-            src={`${process.env.NEXT_PUBLIC_SITE_URL}/images/luckyducks/${images[currentIndex]}`}
-            alt={`Image ${currentIndex + 1}`}
-            width={200}
-            height={200}
-          />
-        )}
+        <Image
+          src={`${process.env.NEXT_PUBLIC_SITE_URL}/images/luckyducks/${images[currentIndex]}`}
+          alt={`Image ${currentIndex + 1}`}
+          width={200}
+          height={200}
+        />
         <button onClick={handleNext} className={styles.navButton}>
           Next
         </button>
@@ -104,6 +75,22 @@ const Frame = () => {
   )
 }
 
+export const metadata: Metadata = {
+  title: "Mint NFT",
+  description: "Mint one of the 777 unique images as an NFT",
+  openGraph: {
+    title: "Mint NFT",
+    description: "Mint one of the 777 unique images as an NFT",
+    images: [
+      `${process.env.NEXT_PUBLIC_SITE_URL}/images/luckyducks/Adam_G.jpg`,
+    ],
+  },
+  other: {
+    ...frameMetadata,
+  },
+}
+
+// Page component
 export default function Page() {
   const images = [
     "Abigail.jpg",
