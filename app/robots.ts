fcd9869a8fecc85cbd861f861738c70b096999da
Michael <enjoyweaver@gmail.com>
2024-08-22 17:45:38 -0500

    layout

--- app/robots.ts
+++ app/robots.ts
@@ -2,9 +2,14 @@ import { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/private", "/admin", "/internal"],
+      },
+    ],
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
+    host: process.env.NEXT_PUBLIC_SITE_URL,
   }
 }
--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from "types"
 export const siteConfig: SiteConfig = {
   name: "SafeMeme Labs",
   description: "Creating a safememe economy",
-  url: "https://safememe.vercel.app",
+  url: "https://safememes.fun",
   ogImage: "https://twitter.com/safememelabs/photo",
   links: {
     twitter: "https://twitter.com/safememelabs",
