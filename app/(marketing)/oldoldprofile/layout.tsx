ba94b89a007c47ef154e5cafb5c731dd3bde53ed
Michael <enjoyweaver@gmail.com>
2024-09-22 17:46:50 -0500

    remove old folders

--- app/(marketing)/oldoldprofile/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next"
-
-import MyProfile from "./page"
-
-export const metadata: Metadata = {
-  title: "My Profile | SafeMeme Labs",
-  description: "Check out my profile at SafeMeme Labs",
-}
-export default MyProfile
